177c992f001352bc664bbd0cb04c8493b3891b89
Gui-PCS <69014770+Gui-PCS@users.noreply.github.com>
2021-11-08 12:58:50 -0300

    Add files via upload

---
+++ Urls e Vidro/link.py
@@ -0,0 +1,16 @@
+def url(n):
+    if(n==1):
+        print("https://youtu.be/jNck3dk31mU")
+    elif(n==2):
+        print("https://youtu.be/TWdUmQkScRY")
+    elif(n==3):
+        print("https://youtu.be/wgPn3kZZtIY")
+    elif(n==4):
+        print("https://youtu.be/4OVW4SRYRp0")
+    elif(n==5):
+        print("https://youtu.be/SsbVy4gr0j4")
+    else:
+        print("Matérial não encontrado")
+
+n=int(input("Digite o número do matérial desejado:\n1)Papel\n2)Plástico\n3)Metal\n4)Vidro\n5)Madeira\n"))
+url(n)
